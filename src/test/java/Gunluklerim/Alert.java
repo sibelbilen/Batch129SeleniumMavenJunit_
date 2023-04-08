70e0d306a1d8eae6f0d33b2f2db11fb636f62226
sibelbilen <sibelinko33@gmail.com>
2023-04-08 13:24:48 +0300

    .

---
+++ src/test/java/Gunluklerim/Alert.java
@@ -0,0 +1,20 @@
+package Gunluklerim;
+
+import Utilities.TestBase;
+import org.junit.Test;
+
+public class Alert extends TestBase {
+
+    @Test
+    public void test01() {
+        //http://demo.guru99.com/test/delete_customer.php adresine gidin
+        //customer id kısmına 53920 yazın
+        //submit butonuna tıklayın
+        //cıkan alert mesajını yazdırın
+        //alerti kabul edin
+        //cıkan 2. alert mesajını yazdırın
+        //cıkan 2. alerti kabul edin
+
+
+    }
+}
--- src/test/java/Gunluklerim/RadioButton.java
+++ src/test/java/Gunluklerim/RadioButton.java
@@ -6,10 +6,33 @@ package Gunluklerim;
 // option1 elementlerinin secili olmadıgını test edin
 // option3 elementlerinin secili olmadıgını test edin
 
+import Utilities.TestBase;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class RadioButton {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RadioButton extends TestBase {
     @Test
     public void name() {
+        // https://demo.guru99.com/test/radio.html adresine gidin
+        driver.get("https://demo.guru99.com/test/radio.html");
+        // radiobutton elementlerini locate edin
+        WebElement option01= driver.findElement(By.xpath("//input[@id='vfb-7-1']"));
+        WebElement option02= driver.findElement(By.xpath("//input[@id='vfb-7-2']"));
+        WebElement option03= driver.findElement(By.xpath("//input[@id='vfb-7-3']"));
+        // option2'yi secin
+        option02.click();
+
+        // option2 elementlerinin secili oldugunu Test edin
+        assertTrue(option02.isSelected());
+        // option1 elementlerinin secili olmadıgını test edin
+        assertFalse(option01.isSelected());
+
+        // option3 elementlerinin secili olmadıgını test edin
+assertFalse(option03.isSelected());
     }
 }
