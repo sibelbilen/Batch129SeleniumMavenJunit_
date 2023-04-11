6a7046f0fb9e4fc9962bcfab861a30373b4575c8
sibelbilen <sibelinko33@gmail.com>
2023-04-11 12:47:19 +0300

    .

--- src/test/java/day15_FileUpload_Waits/C03_FluentWait.java
+++ src/test/java/day15_FileUpload_Waits/C03_FluentWait.java
@@ -4,7 +4,17 @@ import Utilities.TestBase;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
 
 public class C03_FluentWait  extends TestBase {
 /*
@@ -15,14 +25,22 @@ Hello World! Yazının sitede oldugunu test edin
 
     @Test
     public void test01() {
-
-        //https://the-internet.herokuapp.com/dynamic_loading/1
-
+//        https://the-internet.herokuapp.com/dynamic_loading/1
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
-        //Start buttonuna tıklayın
+
+//        Start buttonuna tıklayın
         driver.findElement(By.xpath("//div[@id='start']/button")).click();
-        //Hello World! Yazının sitede oldugunu test edin
-        WebElement helloWordTest=  driver.findElement(By.xpath("//div[@id='finish']/h4"));
-        Assert.assertTrue(helloWordTest.isDisplayed());
+
+//        Hello World! Yazının sitede oldugunu test edin
+        //Fluent wait'in WebDriverWait'ten farkı 'wait' objesinin oluşturulmasında ortaya çıkar.
+        Wait<WebDriver> wait = new FluentWait<>(driver).
+                withTimeout(Duration.ofSeconds(30)).//Max bekleme süresi
+                        pollingEvery(Duration.ofSeconds(3)).//Deneme aralıkları: Her 3 saniyede bir bulma denemesi yapar
+                        withMessage("Ignore Exception").//Mesaj yazdırılabilir: Zorunlu değil.
+                        ignoring(TimeoutException.class);//Exception handle edilebilir: zorunlu değil.
+
+        WebElement helloWorldText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='finish']//h4")));
+
+        assertTrue(helloWorldText.isDisplayed());
     }
 }
