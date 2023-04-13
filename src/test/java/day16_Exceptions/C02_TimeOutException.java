54e69545edabf3ea951b72fbb9808177cde21f1c
sibelbilen <sibelinko33@gmail.com>
2023-04-13 11:49:46 +0300

    .

--- src/test/java/day16_Exceptions/C02_TimeOutException.java
+++ src/test/java/day16_Exceptions/C02_TimeOutException.java
@@ -3,13 +3,22 @@ package day16_Exceptions;
 import Utilities.TestBase;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class C02_TimeOutException extends TestBase {
 
     @Test
     public void TimeOutExceptionTest() {
-        driver.get("https://www.techproeducation.com/");
+        driver.get("https://www.techproeducation.com");
         driver.findElement(By.xpath("//i[@class='eicon-close']")).click();//Reklamı kapatıyorum
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("Yanlış Locator")));//org.openqa.selenium.TimeoutException
+        //TimeoutException explicit wait kullanımında web element bulunamadığı zaman alınır.
 
 
     }
---
+++ src/test/java/day16_Exceptions/C03_NullPointerException.java
@@ -0,0 +1,16 @@
+package day16_Exceptions;
+
+import Utilities.TestBase;
+import org.junit.Test;
+
+public class C03_NullPointerException  {
+    @Test
+    public void test() {
+
+
+
+
+
+
+    }
+}
