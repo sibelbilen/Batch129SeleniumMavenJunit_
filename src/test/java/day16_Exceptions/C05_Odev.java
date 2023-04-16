0bce6bd70a7fe0cfe91588e6aef50d3970311018
sibelbilen <sibelinko33@gmail.com>
2023-04-16 12:22:46 +0300

    .

--- src/test/java/day16_Exceptions/C05_Odev.java
+++ src/test/java/day16_Exceptions/C05_Odev.java
@@ -12,11 +12,41 @@ public class C05_Odev {
 
     public static class Odev extends TestBase {
         @Test
+        public void StaleElementReferanceExceptionTest5() {
+
+            //NOT:StaleElementReferanceException hatası almamız gerekiyor
+            //Amazon sayfasına gidelim
+            driver.get("https://amazon.com");
+
+            //iphone aratalım
+            driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+
+            //Çıkan sonuclardan ilk 5'ine tıklayıp sayfa başlıklarını alalım konsola yazdıralım
+            List<WebElement> urunler = driver.findElements(By.xpath("//h2"));
+            for (int i = 0; i < urunler.size(); i++) {
+                urunler.get(i).click();
+                bekle(3);
+                driver.navigate().back();
+                bekle(3);
+                if (i == 4) {
+                    break;
+                }
+            }
+
+
+            //Sayfayı kapatalım
+
+        }
+
+
+        @Test
         public void StaleElementReferanceExceptionTest6() {
             //Amazon sayfasına gidelim
             driver.get("https://amazon.com");
+
             //iphone aratalım
             driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+
             //Çıkan sonuclardan ilk 5'ine tıklayıp sayfa başlıklarını alalım konsola yazdıralım
             List<WebElement> urunler = driver.findElements(By.xpath("//h2"));
             for (int i = 0; i < urunler.size(); i++) {
