c0d0b9f8f6cd4cc998f566065f04d2fef86cb785
sibelbilen <sibelinko33@gmail.com>
2023-04-16 22:17:41 +0300

    .

--- src/test/java/day18_ScreenShot/ScreenShot.java
+++ src/test/java/day18_ScreenShot/ScreenShot.java
@@ -7,8 +7,16 @@ public class ScreenShot extends TestBase {
     @Test
     public void test01() {
 
+//Techproeducation sayfasına gidelim
+        driver.get("https://techproeducation.com");
 
 
+//Sayfanın resmini alalım
+    tumSayfaResmi();
+//Sonra amazon sayfasına gidelim
+        driver.get("http://amazon.com");
+//Amazon sayfasının ekran görüntüsünü alalım
 
+tumSayfaResmi();
     }
 }
