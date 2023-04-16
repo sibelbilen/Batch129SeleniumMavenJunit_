527c853711866b60daaa3477f1273fc103bc8221
sibelbilen <sibelinko33@gmail.com>
2023-04-16 22:12:40 +0300

    .

--- src/test/java/Utilities/TestBase.java
+++ src/test/java/Utilities/TestBase.java
@@ -1,25 +1,30 @@
 package Utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public abstract class TestBase {
-    //TestBase class'indan obje olusturmanin onune gecilmesi icin abstract yapilabilir
-//Orn: TestBase base=new TestBase();
-//Bu class'a extend ettigimiz test classlarinda ulasabiliriz.
+    //TestBase class'ından Obje oluşturmanın önüne geçilmesi için abstract yapılabilir
+    //Orn: TestBase base = new TestBase()
+    //Bu class'a extends ettiğimiz test classlarından ulaşabiliriz
     protected static WebDriver driver;
-
     @Before
     public void setUp() throws Exception {
         WebDriverManager.chromedriver().setup();
@@ -27,76 +32,89 @@ public abstract class TestBase {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
-
     @After
     public void tearDown() throws Exception {
-  Thread.sleep(3000);
-       //  driver.quit();
+        bekle(3);
+        //driver.quit();
     }
-
     //HARD WAIT METHOD
-    public static void bekle(int saniye) {
+    public static void bekle(int saniye){
         try {
-            Thread.sleep(saniye * 1000);
+            Thread.sleep(saniye*1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
-
     //Alert ACCEPT
-    public static void alertAccept() {
+    public static void alertAccept(){
         driver.switchTo().alert().accept();
     }
-
     //Alert DISMISS
-    public static void alertDismiss() {
+    public static void alertDismiss(){
         driver.switchTo().alert().dismiss();
     }
-
     //Alert getText()
-    public static void alertText() {
+    public static void alertText(){
         driver.switchTo().alert().getText();
     }
-
     //Alert promptBox
-    public static void alertprompt(String text) {
+    public static void alertprompt(String text){
         driver.switchTo().alert().sendKeys(text);
     }
-
     //DropDown VisibleText
     /*
         Select select2 = new Select(gun);
         select2.selectByVisibleText("7");
-
         //ddmVisibleText(gun,"7"); --> Yukarıdaki kullanım yerine sadece method ile handle edebilirim
      */
-    public static void ddmVisibleText(WebElement ddm, String secenek) {
+    public static void ddmVisibleText(WebElement ddm,String secenek){
         Select select = new Select(ddm);
         select.selectByVisibleText(secenek);
-
     }
-
     //DropDown Index
-    public static void ddmIndex(WebElement ddm, int index) {
+    public static void ddmIndex(WebElement ddm,int index){
         Select select = new Select(ddm);
         select.selectByIndex(index);
     }
-
     //DropDown Value
-    public static void ddmValue(WebElement ddm, String secenek) {
+    public static void ddmValue(WebElement ddm,String secenek){
         Select select = new Select(ddm);
         select.selectByValue(secenek);
     }
-
-
-    //SwitchTo: Sayfalar arası geçiş methodu:
-    //Indeks 0'dan başlar
-    //Girilen indeksteki windowHandle değerini alarak o sayfaya geçiş yapar.
-    public static void switchToWindow(int sayfaIndeksi) {
-
-        List<String> windowHandleList = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(windowHandleList.get(sayfaIndeksi));
-
-
+    //SwitchToWindow
+    public static void switchToWindow(int sayi){
+        List<String> tumWindowHandles = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tumWindowHandles.get(sayi));
+    }
+    //SwitchToWindow2
+    public static void window(int sayi){
+        driver.switchTo().window(driver.getWindowHandles().toArray()[sayi].toString());
+    }
+    //EXPLICIT WAIT METHODS
+    //Visible Wait
+    public static void visibleWait(WebElement element,int sayi){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(sayi));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    //VisibleElementLocator Wait
+    public static WebElement visibleWait(By locator, int sayi){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(sayi));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    //Alert Wait
+    public static void alertWait(int sayi){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(sayi));
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+    //Tüm Sayfa ScreenShot
+    public static void tumSayfaResmi(){
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "TestOutput/screenshot"+tarih+".png";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        try {
+            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/day18_ScreenShot/ScreenShot.java
@@ -0,0 +1,14 @@
+package day18_ScreenShot;
+
+import Utilities.TestBase;
+import org.junit.Test;
+
+public class ScreenShot extends TestBase {
+    @Test
+    public void test01() {
+
+
+
+
+    }
+}
