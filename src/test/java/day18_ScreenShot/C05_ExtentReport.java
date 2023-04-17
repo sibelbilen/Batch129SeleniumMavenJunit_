bfc586878d9a2ecf16d876ef39039d27760aa115
sibelbilen <sibelinko33@gmail.com>
2023-04-17 11:05:33 +0300

    .

--- src/test/java/day18_ScreenShot/C05_ExtentReport.java
+++ src/test/java/day18_ScreenShot/C05_ExtentReport.java
@@ -1,9 +1,36 @@
 package day18_ScreenShot;
 
 import Utilities.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 public class C05_ExtentReport extends TestBase {
+    @Test
+    public void test() {
+        extentTest=extentReports.createTest("ExtentTest","Test Raporu");
+        driver.get("https://techproeducation.com");
+        extentTest.info("Tecproeducation sayfasına gidildi");
+        bekle(3);
+        extentTest.info("3 saniye bekletildi");
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click();//Reklamı kapatıyorum
+        extentTest.info("Çıkan reklam kapatıldı");
+        //sayfanın resmini alalım
+        tumSayfaResmi();
+        extentTest.info("Tüm sayfanın resmi alındı");
+        //Arama bölümünde java aratalım
+        driver.findElement(By.xpath("//input[@type='search']")).sendKeys("java", Keys.ENTER);
+        extentTest.info("Arama bölümünde java aratıldı");
 
+        //ve sonuc yazısının resmini alalım
+        WebElement sonucYazisi = driver.findElement(By.xpath("//h1"));
+        webElementResmi(sonucYazisi);//Method kullanarak Webelementin resmini aldık
+        extentTest.info("Sonuc yazısının resmi alındı");
+        extentTest.pass("Test sonlandırıldı");
+
+
+    }
 
 
 }
---
+++ src/test/java/day19_WebTables_ReadExcel/C01_WebTables.java
@@ -0,0 +1,4 @@
+package day19_WebTables_ReadExcel;
+
+public class C01_WebTables {
+}
