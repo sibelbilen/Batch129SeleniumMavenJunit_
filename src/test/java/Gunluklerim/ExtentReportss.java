7ad69e56fe2401779d18a2649937d1a99533abf0
sibelbilen <sibelinko33@gmail.com>
2023-04-18 20:26:34 +0300

    .

--- src/test/java/Gunluklerim/ExtentReportss.java
+++ src/test/java/Gunluklerim/ExtentReportss.java
@@ -12,35 +12,44 @@ import org.openqa.selenium.WebElement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class ExtentReportss extends TestBase {
-    ExtentReports extentReports;//====>>>raporlamyi baslatir.
-    ExtentHtmlReporter extentHtmlReporter;//====>>raporlari html formatinda duzenler.
-    ExtentTest extentTest;//==>tum test asamalarinda extentTest classindan bir obje olusturur.
+ExtentReports extentReports;//--->raporlamayi baslatir.
+    ExtentHtmlReporter extentHtmlReporter;//----->raporlamyi html formatinda duzenler.
+    ExtentTest extentTest;//--->Tum Test asamalarinda extest objesi ile bilgi ekleriz
 
     @Test
-    public void extentReport() {
-     extentReports = new ExtentReports();
-        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
-        String dosyaYolu = "TestOutput/reports/extentReportTEKRAR"+tarih+".html";
+    public void name() {
+        extentReports=new ExtentReports();
+        String tarih=new SimpleDateFormat("_hh_ss_dd_dd/mm/yyyy").format(new Date());
+        String dosyaYolu="TestOutput/reports/sibelTekrar"+tarih;
         extentHtmlReporter=new ExtentHtmlReporter(dosyaYolu);
+        extentReports.attachReporter(extentHtmlReporter);
 
-extentReports.attachReporter(extentHtmlReporter);
+        //raporda ggozukmaisni istedigimiz bilgiler
+        extentReports.setSystemInfo("chrome","edge");
+        extentReports.setSystemInfo("tester","sibel guler");
+        extentHtmlReporter.config().setReportName("bakalim nerede");
+        extentHtmlReporter.config().setReportName("rapor ismi herhalde");
+        extentTest=extentReports.createTest("test ismi","test ra");
 
-extentReports.setSystemInfo("chrome","edge");
-extentReports.setSystemInfo("QA","sibel guler");
-extentHtmlReporter.config().setReportName("extent rapor");
-extentHtmlReporter.config().setReportName("smoke test");
-extentTest=extentReports.createTest("bakim");
-driver.get("https://amazon.com");
-extentTest.info("amazon sayfasina gidilid");
+
+
+        driver.get("https://amazon.com");
+
+        extentTest.info("amazon sayfasina giris yapildi");
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
-        extentTest.info("Amazon sayfasında iphone araması yapıldı");
+        extentTest.info("amazon sayfasinda iphone aramasi yapildi");
         WebElement sonucYazisi = driver.findElement(By.xpath("(//*[@class='sg-col-inner'])[1]"));
         System.out.println(sonucYazisi.getText());
-        extentTest.info("Sonuc yazısı konsola yazdırıldı");
-        extentTest.pass("Sayfa kapatıldı");
+        extentTest.info("test yazisi konsola yazildi");
+        extentTest.pass("sayfa kapatildi");
+
+
+
+
+
         extentReports.flush();
-
-
     }
 }
+
