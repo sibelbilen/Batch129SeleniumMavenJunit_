604bbcbc0f46f050771d5e134d7a3bfd66c627e4
sibelbilen <sibelinko33@gmail.com>
2023-04-22 19:22:54 +0300

    .

---
+++ src/test/java/Gunluklerim/ExcelWhite.java
@@ -0,0 +1,8 @@
+package Gunluklerim;
+
+public class ExcelWhite {
+         /*
+    Bir "NÜFUS" sütunu oluşturup başkent nüfuslarını excel doyasına yazınız:
+    (D.C: 712816, Paris: 2161000, London: 8982000, Ankara: 5663000 ...)
+     */
+}
