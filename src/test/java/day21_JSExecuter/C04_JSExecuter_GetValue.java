f42b02878917af9fecad19ac88bdecede2c87dc9
sibelbilen <sibelinko33@gmail.com>
2023-04-24 13:33:00 +0300

    .

---
+++ src/test/java/day21_JSExecuter/C04_JSExecuter_GetValue.java
@@ -0,0 +1,23 @@
+package day21_JSExecuter;
+
+import Utilities.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+public class C04_JSExecuter_GetValue extends TestBase {
+
+// 2)https://www.priceline.com/ a git
+
+//  3)Tarih kısmındaki Yazili metinleri al ve yazdır
+
+
+    @Test
+    public void test01() {
+
+// 2)https://www.priceline.com/ a git
+      driver.get("https://www.carettahotel.com/");
+//  3)Tarih kısmındaki Yazili metinleri al ve yazdır
+
+
+    }
+}
