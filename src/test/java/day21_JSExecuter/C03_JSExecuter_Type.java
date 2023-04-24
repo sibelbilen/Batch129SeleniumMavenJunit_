7393f16e0dad67f8a07bee0c787e4118b349f47f
sibelbilen <sibelinko33@gmail.com>
2023-04-24 12:47:53 +0300

    .

--- src/test/java/day21_JSExecuter/C03_JSExecuter_Type.java
+++ src/test/java/day21_JSExecuter/C03_JSExecuter_Type.java
@@ -1,4 +1,25 @@
 package day21_JSExecuter;
 
-public class C03_JSExecuter_Type {
+import Utilities.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class C03_JSExecuter_Type extends TestBase {
+   /*
+Techpro education ana sayfasina git
+Arama kutusuna QA yaz
+ */
+
+    @Test
+    public void test01() {
+
+        //Techpro education ana sayfasina git
+        //        Techpro education ana sayfasına git
+        driver.get("https://techproeducation.com/");
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click();
+
+
+        //Arama kutusuna QA yaz
+
+    }
 }
