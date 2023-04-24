e3a1a7efac430e055d69a914c34570054e7fb8ab
sibelbilen <sibelinko33@gmail.com>
2023-04-24 12:17:12 +0300

    .

--- src/test/java/day21_JSExecuter/C02_JSExecuter_Scroll.java
+++ src/test/java/day21_JSExecuter/C02_JSExecuter_Scroll.java
@@ -1,4 +1,39 @@
 package day21_JSExecuter;
 
-public class C02_JSExecuter_Scroll {
-}
+import Utilities.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class C02_JSExecuter_Scroll extends TestBase {
+    /*
+Techpro education ana sayfasına git
+We Offer elementi görünür olacak şekilde üzerine scroll et ve ekran görüntüsünü al
+Aynı sayfada Enroll Free elementi görünür olacak sekilde scroll et ve ekran görüntüsünü al
+Aynı sayfada WHY US? elementi görünür olacak şekilde scroll et ve ekran görüntüsünü al
+Aynı sayfada tekrar Enroll Free elementi görünür olacak şekilde scroll et ve ekran görüntüsünü al
+ */
+
+    @Test
+    public void test01() {
+//        Techpro education ana sayfasına git
+        driver.get("https://techproeducation.com/");
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click();
+
+//        "We Offer" elementi görünür olacak şekilde üzerine scroll et ve ekran görüntüsünü al
+        WebElement weOffer = driver.findElement(By.xpath("//span[.='we offer']"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",weOffer);
+
+//        Aynı sayfada "Enroll Free "elementi görünür olacak sekilde scroll et ve ekran görüntüsünü al
+
+//        Aynı sayfada "WHY US?" elementi görünür olacak şekilde scroll et ve ekran görüntüsünü al
+//        Aynı sayfada tekrar "Enroll Free" elementi görünür olacak şekilde scroll et ve ekran görüntüsünü al
+
+    }
+
+    }
+
+
