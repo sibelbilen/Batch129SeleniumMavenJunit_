daf38a7bf7cc4ea6f0044e7837dab3af7067eda0
sibelbilen <sibelinko33@gmail.com>
2023-04-25 11:25:13 +0300

    .

--- src/test/java/day22_Log4J/C01_Log4J.java
+++ src/test/java/day22_Log4J/C01_Log4J.java
@@ -1,4 +1,21 @@
 package day22_Log4J;
 
-public class C01_Log4J {
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class C01_Log4J  {
+
+   private  static Logger logger= LogManager.getLogger(C01_Log4J.class.getName()) ;
+
+    @Test
+    public void log4jTest() {
+
+        //Logger fonksiyonuyla istenilen fonksiyonu kullan
+        logger.debug("Debug logger");
+        logger.info("Info logger");
+        logger.error("Error logger");
+        logger.fatal("Fatal logger");
+    }
 }
