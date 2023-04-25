fb4180e44c7e6a1687d94d6dc5cd4fbb0640442b
sibelbilen <sibelinko33@gmail.com>
2023-04-25 18:12:17 +0300

    .

---
+++ src/test/java/Gunluklerim/LOG4J/lOG4J01.java
@@ -0,0 +1,19 @@
+package Gunluklerim.LOG4J;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class lOG4J01 {
+    //logger objesi olsuturduk
+   private static  Logger logger= LogManager.getLogger(lOG4J01.class.getName());
+
+    @Test
+    public void test01() {
+        logger.info("onemli degil");
+        logger.fatal("onemli");
+        logger.error("onemli");
+        logger.debug("onemli degil");
+
+    }
+}
