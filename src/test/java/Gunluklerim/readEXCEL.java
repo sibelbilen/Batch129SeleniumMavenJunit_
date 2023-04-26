91e7fb899565bb80e3d760b9c2e61a4fb2ee7644
sibelbilen <sibelinko33@gmail.com>
2023-04-26 21:53:53 +0300

    .

---
+++ src/test/java/Gunluklerim/readEXCEL.java
@@ -0,0 +1,14 @@
+package Gunluklerim;
+
+import org.junit.Test;
+
+public class readEXCEL {
+    // Ulkeler dosyasındaki 3.satırdaki indexin, 3.sutunundaki indexinin
+    // "Cezayir" oldugunu test eden method olusturunuz
+
+
+    @Test
+    public void name() {
+
+    }
+}
